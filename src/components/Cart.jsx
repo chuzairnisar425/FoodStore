4d3ef9070fa1cd74b5d2a8ddb83ff263ace9bef3
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-17 13:11:32 +0500

    modified

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import ItemCard from "./ItemCard";
+import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
 function Cart() {
+  const [activeCart, setActiveCart] = useState(true);
+  const cartItems = useSelector((state) => state.cart.cart);
+  // console.log(cartItems);
+
   return (
     <>
-      <div className="fixed right-0 top-0 w-11/12 sm:w-11/12 md:w-[40vw] lg:w-[20vw]  h-full bg-white p-5 mb-3">
-        <div className="flex justify-between items-center my-3">    
+      <div
+        className={`fixed right-0 top-0 w-11/12 sm:w-11/12 md:w-[40vw] lg:w-[20vw] h-full bg-white p-5 mb-3  duration-500 transition-all ease-in-out  ${activeCart ? "duration-500 transition-all ease-in-out" : " hidden"
+          }`}
+      >
+        <div className="flex justify-between items-center my-3">
           <span className="text-xl - font-bold text-gray-800">My Order</span>
           <IoMdClose
+            onClick={() => setActiveCart(!activeCart)}
             className="border-2 outline cursor-pointer border-gray-600 text-gray-600 font-bold p-1
           hover:text-red-300 hover:border-red-300 text-xl rounded-md "
           />
-
         </div>
-<ItemCard/>
+        {cartItems.length > 0 ? cartItems.map((food) => {
+          return (
+            <ItemCard
+              key={food.id}
+              id={food.id}
+              name={food.name}
+              price={food.price}
+              img={food.img}s
+              qty={food.qty}
+            />
+          );
+        }):<h2 className="text-center text-xl font-bold text-gray-800">Your cart is empty</h2>}
 
         <div className="absolute bottom-0 mb-20">
           <h3 className="font-semibold text-gray-800">Items :</h3>
@@ -24,6 +45,10 @@ function Cart() {
           </button>
         </div>
       </div>
+      <FaShoppingCart
+        onClick={() => setActiveCart(!activeCart)}
+        className="rounded-lg bg-white shadow-md text-3xl p-3 fixed bottom-4 right-4"
+      />
     </>
   );
 }
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
-function ItemCard() {
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/slices/CartSlices";
+function ItemCard({ id, name, price, img, qty }) {
+  const dispatch = useDispatch();
   return (
     <div className="flex items-center  gap-2 shadow-md rounded-lg p-5">
-      <MdDelete className="absolute right-7 cursor-pointer text-gray-800 "/>
+      <MdDelete onClick={() => dispatch(removeFromCart({id  }))}
+        className="absolute right-7 text-2xl cursor-pointer text-gray-800 " />
       <img
-        src="https://w7.pngwing.com/pngs/356/675/png-transparent-falafel-lebanese-cuisine-take-out-mediterranean-cuisine-hummus-bed-miscellaneous-furniture-food-thumbnail.png"
+        src={img}
         alt=""
         className="w-[50px] h-[50px]"
       />
 
       <div className="leading-5">
-        <h2 className="font-bold text-gray-800 ">Onion Pizza</h2>
-        
-          <div className="flex items-center justify-between">
-            <span className="text-green-500 font-semibold">$120</span>{" "}
-        <div className="flex justify-center items-center gap-2 absolute right-7 ">
-          <AiOutlinePlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md duration-300 text-xl p-1  transition-all ease-linear cursor-pointer outline" />
-          <span>1</span>
-          <AiOutlineMinus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md duration-300 text-xl p-1  transition-all ease-linear cursor-pointer outline" />
-        </div>
+        <h2 className="font-bold text-gray-800 ">{name}</h2>
+
+        <div className="flex items-center justify-between">
+          <span className="text-green-500 font-semibold">{price}</span>{" "}
+          <div className="flex justify-center items-center gap-2 absolute right-7 ">
+            <AiOutlinePlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md duration-300 text-xl p-1  transition-all ease-linear cursor-pointer outline" />
+            <span>{qty}</span>
+            <AiOutlineMinus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md duration-300 text-xl p-1  transition-all ease-linear cursor-pointer outline" />
+          </div>
         </div>
       </div>
     </div>
