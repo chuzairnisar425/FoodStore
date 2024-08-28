4ffd5481aa5c20951a2f53cbe857083a1ce43506
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-28 11:44:02 +0500

    modified

---
+++ Chillis-Backend/models/Food.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const foodSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    price: Number,
+    totalPrice: Number,
+    quantity: Number,
+    rating: Number,
+    image: String,
+    userId: String,
+}, { timestamps: true })
+
+export const Food = mongoose.model('food', foodSchema);
