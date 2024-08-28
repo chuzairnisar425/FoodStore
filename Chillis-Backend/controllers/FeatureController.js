85bd27004ccffc67d1a87339c1a29a67fe0d125e
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-28 17:00:52 +0500

    modified

--- Chillis-Backend/controllers/FeatureController.js
+++ Chillis-Backend/controllers/FeatureController.js
@@ -47,19 +47,24 @@ export const addToCart = async (req, res) => {
             totalPrice: price * quantity,
         });
         const savedFood = await newFood.save();
-        let user = await User.findOneAndUpdate({ _id: userId }, {
-            $push: {
-                cartItems: savedFood._id,
+        let user = await User.findOneAndUpdate(
+            { _id: userId },
+            {
+                $push: {
+                    cartItems: savedFood._id,
+                },
             }
-        })
+        );
         if (!user) {
             return res.status(400).json({
-                success: false, message: 'Failed to add to cart'
-            })
+                success: false,
+                message: "Failed to add to cart",
+            });
         }
         return res.status(200).json({
-            success: true, message: ' Added to cart'
-        })
+            success: true,
+            message: " Added to cart",
+        });
     } catch (error) {
         return res.status(500).json({
             success: true,
@@ -68,31 +73,31 @@ export const addToCart = async (req, res) => {
     }
 };
 
-
 // get cart route
 
 export const getCart = async (req, res) => {
     const userId = req.params.id;
 
     try {
-        const cartItems = await Food.find({ userId })
+        const cartItems = await Food.find({ userId });
 
         if (!cartItems) {
             return res.status(400).json({
-                success: false, message: 'No items found'
-            })
+                success: false,
+                message: "No items found",
+            });
         }
         return res.status(200).json({
-            success: true, cartItems
-        })
+            success: true,
+            cartItems,
+        });
     } catch (error) {
         return res.status(500).json({
             success: true,
             message: error.message,
         });
     }
-
-}
+};
 
 // Remove from cart route
 
@@ -100,65 +105,115 @@ export const removeFromCart = async (req, res) => {
     const id = req.params.id;
     try {
         let food = await Food.findOneAndDelete({
-            _id: id
-        })
+            _id: id,
+        });
         if (!food) {
             return res.status(400).json({
                 success: false,
-                message: 'Food not found'
-            })
-
+                message: "Food not found",
+            });
         }
         return res.status(200).json({
             success: true,
-            message: 'Food removed from cart'
-        })
-
+            message: "Food removed from cart",
+        });
     } catch (error) {
         return res.status(500).json({
-            success: false, message: error.message
-        })
+            success: false,
+            message: error.message,
+        });
     }
-}
+};
 
 // increment quantity
 
 export const incrementQuantity = async (req, res) => {
     const id = req.params.id;
     try {
-
-        let food = await Food.findOneAndUpdate({
-            _id: id
-        },
+        let food = await Food.findOneAndUpdate(
+            {
+                _id: id,
+            },
             {
                 $set: {
-                    quantity: { $add: ['$quantity', 1] },
+                    quantity: { $add: ["$quantity", 1] },
                     totalPrice: {
-                        $multiply: ['$price', {
-                            $add: ['$quantity', 1]
-                        }]
-                    }
-                }
+                        $multiply: [
+                            "$price",
+                            {
+                                $add: ["$quantity", 1],
+                            },
+                        ],
+                    },
+                },
             },
             {
                 upsert: true,
                 new: true,
             }
-        )
+        );
         if (!food) {
             return res.status(400).json({
                 success: false,
-                message: 'Food not found'
-            })
+                message: "Food not found",
+            });
         }
         return res.status(200).json({
             success: true,
-            message: 'Food quantity incremented', food
-        })
+            message: "Food quantity incremented",
+            food,
+        });
     } catch (error) {
         return res.status(500).json({
-            success: false, message: error.message
-        })
-
+            success: false,
+            message: error.message,
+        });
     }
-}
+};
+
+// decrement quantity route
+
+export const decrementQuantity = async (req, res) => {
+    const id = req.params.id;
+    try {
+        let food = await Food.findOneAndUpdate(
+            {
+                _id: id,
+                quantity: { $gt: 0 },
+            },
+            {
+                $set: {
+                    quantity: {
+                        $subtract: ["$quantity", 1],
+                    },
+                    totalPrice: {
+                        $subtract: ["$totalPrice", "$price"]
+                    },
+                },
+            },
+            {
+                upsert: true,
+                new: true,
+            }
+        );
+
+        if (!food) {
+            return res.status(400).json({
+                success: false,
+                message: "Food not found or quantity already at the maximum",
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Food quantity decremented",
+            food,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+
