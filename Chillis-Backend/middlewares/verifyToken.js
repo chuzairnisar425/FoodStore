90b5d30c78fea19ff68fdc80051e69f037e09f02
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-28 11:43:58 +0500

    modified

--- Chillis-Backend/middlewares/verifyToken.js
+++ Chillis-Backend/middlewares/verifyToken.js
@@ -1,5 +1,20 @@
 import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            success: false, message: 'Unauthorized'
+        })
+    }
+    try {
+        const decoded = jwt.verify(token.proces.env.JWT_SECRET);
+        req.id = decoded.id;
+        next();
 
+    } catch (error) {
+        return res.status(500).json({
+            success: false, message: error.message
+        })
+    }
 }
