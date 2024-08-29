4642b03390ee61293f060162ebd7f5216b62be58
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-29 13:07:02 +0500

    modified

--- Chillis-Frontend/src/components/Navbar.jsx
+++ Chillis-Frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setSearch } from "../redux/slices/SearchSlice";
-
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdClose } from 'react-icons/md'
 const Navbar = () => {
-
-  const dispatch =useDispatch();
+  const [toggleNav, setToggleNav] = useState(false)
+  const dispatch = useDispatch();
   return (
     <nav className="flex flex-col lg:flex-row justify-between mx-6 py-3 mb-3">
       <div className="">
@@ -13,7 +14,7 @@ const Navbar = () => {
       </div>
       <div>
         <input
-        onChange={(e)=>dispatch(setSearch(e.target.value))}
+          onChange={(e) => dispatch(setSearch(e.target.value))}
           type="search"
           name="search"
           id=""
@@ -22,6 +23,11 @@ const Navbar = () => {
           className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
         />
       </div>
+      <GiHamburgerMenu className={`absolute top-5 right-5 lg:right-8 lg:top-6 text-xl text-gray-600 cursor-pointer ${toggleNav && 'hidden'
+        } transition-all duration-500 ease-in-out
+      `} onClick={() => setToggleNav(true)} /><MdClose className={`absolute top-5 right-5 lg:right-8 lg:top-6 text-xl text-gray-600 cursor-pointer ${!toggleNav && 'hidden'
+        } transition-all duration-500 ease-in-out
+      `} onClick={() => setToggleNav(false)} />
     </nav>
   );
 };
